29fb55a054e7140e15cb5784393f8b98cb978495
Near <szewei2004@gmail.com>
2024-03-15 01:07:21 +0800

    Added password hash

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-
-    if (username === "admin" && password === "notadmin") {
-        window.location.href = "dashboard.html";
+  
+    // Hash the password using CryptoJS
+    var hashedPassword = CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex);
+  
+    if (username === "admin" && hashedPassword === "44b68f9bd1c1569c13c9938c2cc7189c4bbe433c0e08f1042e49eedf5b4b1b6b") {
+      window.location.href = "dashboard.html";
     } else {
-        alert("Invalid credentials. Please try again.");
+      alert("Invalid credentials. Please try again.");
     }
-}
-
-// Yes I know that you can see the password
+  }
+  
